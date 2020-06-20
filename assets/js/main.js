73ae0bf0b7752946789d0b94ab2ba909af948cdc
GabielFemi <gabrielfemi799@gmail.com>
2020-06-20 06:52:21 +0100

    Refactor code and add event listener to listen to use Ctrl + enter to submit form when the fields are not empty
    Took 46 minutes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-alert("Hey I made it to the quoote App. Thanks developer Akinyosoye!");
+document.addEventListener("keypress", function (event) {
+
+    if (event.ctrlKey && event.keyCode === 13) {
+        submitForm();
+
+    }
+});
+
+function submitForm () {
+    let quoteTitle = document.getElementById("quote_title").value;
+    let quoteBody = document.getElementById("quote_body").value;
+    if (quoteTitle.length > 0 && quoteBody.length > 0) {
+        document.quooote_form.submit();
+    }
+}
